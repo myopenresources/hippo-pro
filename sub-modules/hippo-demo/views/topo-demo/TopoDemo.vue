e91aeb6e9dbc32e11a6a63da7c170dc4ec31c92d
chenjianghai <332557712@qq.com>
2023-06-28 14:20:44 +0800

    demo

--- sub-modules/hippo-demo/views/topo-demo/TopoDemo.vue
+++ sub-modules/hippo-demo/views/topo-demo/TopoDemo.vue
@@ -1,17 +1,16 @@
 <template>
   <v-scale-screen
-      :width="scaleScreenOptions.width"
-      :height="scaleScreenOptions.height"
-      :delay="scaleScreenOptions.delay"
-      :boxStyle="scaleScreenOptions.boxStyle"
-    >
-      <div class="topo">
-        <svg class="svg-graph" id="topo-graph-2023-6-27-17-32-59">
-          <defs></defs>
-          <g></g>
-        </svg>
-      </div>
-    </v-scale-screen>
+    :width="scaleScreenOptions.width"
+    :height="scaleScreenOptions.height"
+    :delay="scaleScreenOptions.delay"
+    :boxStyle="scaleScreenOptions.boxStyle"
+    style="position: relative; z-index: 1"
+  >
+    <svg class="topo" id="topo-graph-2023-6-27-17-32-59">
+      <defs></defs>
+      <g></g>
+    </svg>
+  </v-scale-screen>
 </template>
 
 <script setup lang="ts">
@@ -23,13 +22,14 @@ import topoData from './data'
 
 const graph = ref<any>(null)
 const tooltip = ref<any>(null)
+const topoAnimationInterval = ref()
 
 const scaleScreenOptions = {
-  width: 1920,
-  height: 1080,
+  width: 850,
+  height: 410,
   delay: 100,
   boxStyle: {
-    backgroundColor: '#fff'
+    backgroundColor: 'transparent'
   }
 }
 
@@ -40,11 +40,10 @@ const drawNode = () => {
       style: 'fill:none',
       labelType: 'html',
       label: `
-        <div style="cursor: pointer; position: relative;margin-top:0px;right:-8px">
-            <img src="${item.image}" style="width:50px;height:50px;transform:rotate(0deg);pointer-events: all;"/>
-
+        <div style="position: relative;">
+            <img src="${item.image}" style="width:100px;height:100px;pointer-events: all;"/>
         </div>
-        <div style="font-size:1.25rem;margin-top:-30px;margin-right:0px;color:#fff;text-align:center;">
+        <div style="font-size:1.2rem;color:#f5faff;text-align:center;">
           ${item.name}
         </div>
       `,
@@ -62,8 +61,7 @@ const drawNode = () => {
 }
 
 const drawLine = () => {
-  const color = '#de0614'
-  const opacity = 1
+  const color = '#187edd'
 
   topoData.lines.forEach((item: any, index: number) => {
     graph.value.setEdge(item.source, item.target, {
@@ -73,19 +71,18 @@ const drawLine = () => {
       lableoffset: 0,
       label: '',
       lineInterpolate: 'basis',
-      class: ``,
-      style: `stroke: ${color}; fill: none;opacity:${opacity};stroke-width:${
-        item.weight ? item.weight : 10
-      };quarters: 0`,
+      class: `speed${item.speed}`,
+      style: `stroke: ${color}; fill: none;
+      stroke-width:${item.weight ? item.weight : 10};quarters: 0`,
       arrowheadStyle: `fill: none;stroke: none;stroke-width:0.1`,
       arrowhead: 'undirected',
       id: item.source + '-' + item.target,
       labelType: 'html',
       curve: d3.curveBasis,
-      width: 80,
-      height: 80,
+      width: 85,
+      height: 85,
       rx: 0,
-      ry: 80
+      ry: 85
     })
   })
 }
@@ -97,10 +94,18 @@ const drawLineAnimationPath = (svg: any) => {
     .selectAll('.edgePath')
     .nodes()
     .forEach((node: any, index: number) => {
+      const nodeCls = node.getAttribute('class')
       let speed = 3
+      if (nodeCls) {
+        const clsList = nodeCls.split(' ')
+        speed = parseFloat(clsList[1].replace('speed', ''))
+      }
+
       const path2 = node.children[0].cloneNode(true)
       path2.id = 'path2' + index
-      path2.style['stroke'] = '#000'
+      path2.style['stroke'] = '#75b1df'
+      path2.style['stroke-width'] =
+        parseFloat(path2.style['stroke-width']) - path2.style['stroke-width'] / 4
       path2.style['opacity'] = 0.85
       path2.style['cursor'] = 'pointer'
 
@@ -112,7 +117,8 @@ const drawLineAnimationPath = (svg: any) => {
       path.id = 'path' + index
       path.setAttribute('class', 'animationPath')
       path.setAttribute('data-speed', speed)
-      path.style['stroke'] = '#000'
+      path.style['stroke-dasharray'] = '5,15'
+      path.style['stroke'] = '#2f80be'
       path.style['stroke-miterlimit'] = 10
       path.style['stroke-width'] =
         parseFloat(path.style['stroke-width']) - path.style['stroke-width'] / 2.5
@@ -133,7 +139,7 @@ const drawLineAnimationPath = (svg: any) => {
       .selectAll('.animationPath')
       .nodes()
       .forEach((item: any) => {
-        if (tmr >= 999) {
+        if (tmr >= 888) {
           tmr = 1
         }
         const speed = parseFloat(item.getAttribute('data-speed'))
@@ -141,11 +147,9 @@ const drawLineAnimationPath = (svg: any) => {
 
         item.style['stroke-dashoffset'] = -offset
       })
-
-    requestAnimationFrame(funAnimation)
   }
 
-  requestAnimationFrame(funAnimation)
+  topoAnimationInterval.value = setInterval(funAnimation, 80)
 }
 
 const initEvent = (svg: any) => {
@@ -174,11 +178,11 @@ const createTooltip = () => {
   tooltip.value = d3
     .select('body')
     .append('div')
-    .style('opacity', 0)
+    .style('opacity', 0.8)
     .style('position', 'absolute')
     .style('font-size', '12px')
     .style('text-align', 'center')
-    .style('background-color', '#0d66aa')
+    .style('background-color', 'rgba(117,117,223,0.8)')
     .style('border-radius', '3px')
     .style('box-shadow', 'rgb(3, 21, 100) 0px 0px 2px')
     .style('padding', '10px')
@@ -192,54 +196,37 @@ const tipHidden = () => {
 }
 
 const tipVisible = (event: any, obj: any) => {
-  const find = getNodeInfoById(obj.id)
-
-  if (find) {
-    tooltip.value.transition().duration(400).style('opacity', 1).style('display', 'block')
-    tooltip.value
-      .html(
-        `<div>
+  tooltip.value.transition().duration(400).style('opacity', 1).style('display', 'block')
+  tooltip.value
+    .html(
+      `<div>
                ${obj.label}
-               <div style="margin-top:5px;color:#fff">${find.name}</div>
         </div>`
-      )
-      .style('left', event.pageX + 15 + 'px')
-      .style('top', event.pageY + 15 + 'px')
-  }
+    )
+    .style('left', event.pageX + 10 + 'px')
+    .style('top', event.pageY + 10 + 'px')
 }
 
-const getNodeInfoById = (id: any) => {
-  const find: any = topoData.nodes.find((item: any) => {
-    return item.id == id
-  })
-  return find
-}
-
-const autoOptimalSize = () => {
-  const paddingVal = 600
+const calcOptimalSize = () => {
   const boxWidth = scaleScreenOptions.width
   const boxHeight = scaleScreenOptions.height
   const graphWidth = graph.value.graph().width
   const graphHeight = graph.value.graph().height
 
-  if (graphWidth <= 1920 || graphHeight <= 1080) {
-    return 1.2
-  }
-
-  let scale: any = 0.6
+  let scale: any = 0.2
   while (true) {
     const newWidth = graphWidth * scale
     const newHeight = graphHeight * scale
 
-    if (newWidth + paddingVal < boxWidth || newHeight + paddingVal < boxHeight) {
-      scale += 0.2
+    if (newWidth < boxWidth || newHeight < boxHeight) {
+      scale += 0.3
     } else {
-      scale -= 0.2
+      scale -= 0.3
       break
     }
   }
 
-  return parseInt(scale)
+  return scale
 }
 
 const zoomToFit = (svg: any, zoom: any) => {
@@ -248,9 +235,10 @@ const zoomToFit = (svg: any, zoom: any) => {
   const graphWidth = graph.value.graph().width
   const graphHeight = graph.value.graph().height
 
-  const scale = autoOptimalSize()
-  const xCenterOffset = (boxWidth - graphWidth * scale) / 2 - 80
-  const yCenterOffset = (boxHeight - graphHeight * scale) / 2 - 120
+  const scale = calcOptimalSize()
+  const xCenterOffset = (boxWidth - graphWidth * scale) / 2
+  const yCenterOffset = (boxHeight - graphHeight * scale) / 2
+
   svg
     .transition()
     .call(
@@ -259,7 +247,6 @@ const zoomToFit = (svg: any, zoom: any) => {
       [0, 0]
     )
 }
-
 
 const initGraph = () => {
   graph.value = new dagreD3.graphlib.Graph({ compound: true })
@@ -274,27 +261,25 @@ const initGraph = () => {
     .setDefaultEdgeLabel(() => {
       return {}
     })
+  initGraphRender()
 }
 
 const initGraphRender = () => {
   if (graph.value) {
-   
     drawNode()
     drawLine()
 
     const svg = d3.select('#topo-graph-2023-6-27-17-32-59')
     const inner = svg.select('g')
 
-    
     const zoom = d3.zoom().on('zoom', (e: any) => {
-      //inner.attr('transform', e.transform + translate.value)
+      inner.attr('transform', e.transform)
     })
 
     svg.call(zoom)
     const render = new dagreD3.render()
     render(inner, graph.value)
 
-    
     drawLineAnimationPath(svg)
     zoomToFit(svg, zoom)
     initEvent(svg)
@@ -303,11 +288,14 @@ const initGraphRender = () => {
 
 onMounted(() => {
   initGraph()
-  initGraphRender()
   createTooltip()
 })
 
-onUnmounted(() => {})
+onUnmounted(() => {
+  if (topoAnimationInterval.value) {
+    clearInterval(topoAnimationInterval.value)
+  }
+})
 </script>
 
 <style scoped lang="scss">
--- sub-modules/hippo-demo/views/topo-demo/data.ts
+++ sub-modules/hippo-demo/views/topo-demo/data.ts
@@ -1,10 +1,10 @@
-const root = ''
-const node1 = ''
-const node2 = ''
-const node3 = ''
-const node4 = ''
-const node5 = ''
-const node6 = ''
+import root from '../../assets/img/topo/root.svg'
+import node1 from '../../assets/img/topo/node1.svg'
+import node2 from '../../assets/img/topo/node2.svg'
+import node3 from '../../assets/img/topo/node3.svg'
+import node4 from '../../assets/img/topo/node4.svg'
+import node5 from '../../assets/img/topo/node5.svg'
+import node6 from '../../assets/img/topo/node6.svg'
 
 let topoData = {
   nodes: [
@@ -81,93 +81,92 @@ let topoData = {
   ],
   lines: [
     {
-      name: '根节点到节点2',
       source: '1',
       target: '2',
-      speed: 50,
-      weight: 30
+      speed: 10,
+      weight: 15
     },
     {
-      name: '节点2到节点3',
       source: '2',
       target: '3',
-      speed: 2,
-      weight: 22
+      speed: 9,
+      weight: 6
     },
     {
-      name: '节点3到节点4',
       source: '3',
       target: '4',
       speed: 15,
-      weight: 20
+      weight: 12
     },
     {
-      name: '节点3到节点5',
       source: '3',
       target: '5',
       speed: 10,
       weight: 13
     },
     {
-      name: '节点4到节点6',
       source: '4',
       target: '41',
       speed: 9,
       weight: 12
     },
     {
-      name: '节点4到节点7',
       source: '4',
       target: '42',
-      speed: 4,
+      speed: 9,
       weight: 9
     },
     {
-      name: '节点6到节点8',
       source: '41',
       target: '411',
-      speed: 21,
+      speed: 12,
       weight: 8
     },
     {
-      name: '节点5到节点11',
       source: '5',
       target: '51',
       speed: 12,
       weight: 12
     },
     {
-      name: '节点5到节点12',
       source: '5',
       target: '52',
       speed: 12,
       weight: 15
     },
     {
-      name: '节点11到节点14',
       source: '51',
       target: '511',
-      speed: 20,
+      speed: 10,
       weight: 10
     },
     {
-      name: '节点11到节点12',
       source: '51',
       target: '52',
       speed: 10,
       weight: 12
     },
     {
-      name: '节点11到节点15',
       source: '51',
       target: '512',
       speed: 10,
       weight: 12
     },
     {
-      name: '节点12到节点15',
       source: '52',
       target: '512',
+      speed: 10,
+      weight: 12
+    },
+    {
+      source: '412',
+      target: '4111',
+      speed: 10,
+      weight: 12
+    },
+    {
+      source: '2',
+      target: '412',
       speed: 10,
       weight: 12
     }
